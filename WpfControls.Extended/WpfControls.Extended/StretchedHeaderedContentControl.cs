ae6231573b8fc5d378a27813010bad2c14f63635
Stormbringer <stormbringer-s@mail.ru>
2014-12-20 23:31:04 +0400

    added stretched headered content control

---
+++ WpfControls.Extended/WpfControls.Extended/StretchedHeaderedContentControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfControls_Extended
+{
+    /// <summary>
+    /// Выполните шаги 1a или 1b, а затем 2, чтобы использовать этот пользовательский элемент управления в файле XAML.
+    ///
+    /// Шаг 1a. Использование пользовательского элемента управления в файле XAML, существующем в текущем проекте.
+    /// Добавьте атрибут XmlNamespace в корневой элемент файла разметки, где он 
+    /// будет использоваться:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:WpfControls_Extended"
+    ///
+    ///
+    /// Шаг 1б. Использование пользовательского элемента управления в файле XAML, существующем в другом проекте.
+    /// Добавьте атрибут XmlNamespace в корневой элемент файла разметки, где он 
+    /// будет использоваться:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:WpfControls_Extended;assembly=WpfControls_Extended"
+    ///
+    /// Потребуется также добавить ссылку из проекта, в котором находится файл XAML,
+    /// на данный проект и пересобрать во избежание ошибок компиляции:
+    ///
+    ///     Щелкните правой кнопкой мыши нужный проект в обозревателе решений и выберите
+    ///     "Добавить ссылку"->"Проекты"->[Поиск и выбор проекта]
+    ///
+    ///
+    /// Шаг 2)
+    /// Теперь можно использовать элемент управления в файле XAML.
+    ///
+    ///     <MyNamespace:StretchedHeaderedContentControl/>
+    ///
+    /// </summary>
+    public class StretchedHeaderedContentControl : HeaderedContentControl
+    {
+        static StretchedHeaderedContentControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(StretchedHeaderedContentControl), new FrameworkPropertyMetadata(typeof(StretchedHeaderedContentControl)));
+        }
+    }
+}
